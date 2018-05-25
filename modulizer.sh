7de53376bd2bbfcf5126bf5b74fa0345820ab899
Paul Varache <paul@kano.me>
2018-05-25 09:33:27 +0100

    Add kwc-icons

--- modulizer.sh
+++ modulizer.sh
@@ -3,4 +3,5 @@ modulizer --out . \
 --npm-version 3.0.0-beta.1 \
 --dependency-mapping kwc-button,@kano/kwc-button,3.0.0-beta.1 \
 --dependency-mapping kwc-style,@kano/kwc-style,3.0.0-beta.1 \
+--dependency-mapping kwc-icons,@kano/kwc-icons,3.0.0-beta.1 \
 --dependency-mapping kwc-behaviours,@kano/kwc-behaviors,3.0.0-beta.1
