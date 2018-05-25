568cd38fc6823509feecad1a73be28e3637e3361
Paul Varache <paul@kano.me>
2018-05-25 09:43:24 +0100

    Spelling

--- modulizer.sh
+++ modulizer.sh
@@ -6,4 +6,4 @@ modulizer --out . \
 --dependency-mapping kwc-icons,@kano/kwc-icons,3.0.0-beta.1 \
 --dependency-mapping kwc-behaviours,@kano/kwc-behaviors,3.0.0-beta.1
 sed -i "s|import '../../@kano/kwc-behaviors/kano-validation.js';|import { Behaviour as ValidationBehavior } from '../../@kano/kwc-behaviors/kano-validation.js';|g" kwc-password-settings.js
-sed -i "s|Kano.Validation.Behaviour'|ValidationBehavior'|g" kwc-password-settings.js
+sed -i "s|Kano.Validation.Behaviour|ValidationBehavior|g" kwc-password-settings.js
