02c1c4e6bd2242c00e72aa0037d351244c461848
Paul Varache <paul@kano.me>
2018-05-25 09:31:58 +0100

    Module

---
+++ modulizer.sh
@@ -0,0 +1,6 @@
+modulizer --out . \
+--npm-name @kano/kwc-password-settings \
+--npm-version 3.0.0-beta.1 \
+--dependency-mapping kwc-button,@kano/kwc-button,3.0.0-beta.1 \
+--dependency-mapping kwc-style,@kano/kwc-style,3.0.0-beta.1 \
+--dependency-mapping kwc-behaviours,@kano/kwc-behaviors,3.0.0-beta.1
