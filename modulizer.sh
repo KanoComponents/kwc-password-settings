ed141784691344477dc68d597f3b5442ee0f8a50
Paul Varache <perso@paulvarache.ninja>
2018-05-26 16:25:35 +0100

    Bump version

--- modulizer.sh
+++ modulizer.sh
@@ -1,6 +1,7 @@
 modulizer --out . \
 --npm-name @kano/kwc-password-settings \
 --npm-version 3.0.0-beta.2 \
+--import-style name \
 --dependency-mapping kwc-button,@kano/kwc-button,3.0.0-beta.2 \
 --dependency-mapping kwc-style,@kano/kwc-style,3.0.0-beta.2 \
 --dependency-mapping kwc-icons,@kano/kwc-icons,3.0.0-beta.2 \
