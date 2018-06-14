7a6437057ffabebd5c653a1116409f5a292df5e2
Paul Varache <perso@paulvarache.ninja>
2018-06-14 17:08:53 +0100

    Bump version

--- modulizer.sh
+++ modulizer.sh
@@ -1,10 +1,10 @@
 modulizer --out . \
 --npm-name @kano/kwc-password-settings \
---npm-version 3.0.0-beta.3 \
+--npm-version 3.0.0-beta.4 \
 --import-style name \
---dependency-mapping kwc-button,@kano/kwc-button,3.0.0-beta.2 \
---dependency-mapping kwc-style,@kano/kwc-style,3.0.0-beta.2 \
---dependency-mapping kwc-icons,@kano/kwc-icons,3.0.0-beta.2 \
---dependency-mapping kwc-behaviours,@kano/kwc-behaviors,3.0.0-beta.2
+--dependency-mapping kwc-button,@kano/kwc-button,^3.0.0-beta.2 \
+--dependency-mapping kwc-style,@kano/kwc-style,^3.0.0-beta.2 \
+--dependency-mapping kwc-icons,@kano/kwc-icons,^3.0.0-beta.2 \
+--dependency-mapping kwc-behaviours,@kano/kwc-behaviors,^3.0.0-beta.4
 sed -i "s|import '@kano/kwc-behaviors/kano-validation.js';|import { Behaviour as ValidationBehavior } from '@kano/kwc-behaviors/kano-validation.js';|g" kwc-password-settings.js
 sed -i "s|Kano.Validation.Behaviour|ValidationBehavior|g" kwc-password-settings.js
